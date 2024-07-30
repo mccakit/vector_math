46f8a7dab92d27645ab8df65a3774313915b9c58
mccakit <mccakit09@gmail.com>
2024-07-30 21:05:04 +0300

    idk

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 int main()
 {
     PPM ppm {256};
-    Vector v{0,100};
-    Matrix m{rotation('z',30,3)};
+    Vector v{0,100,0,1};
+    const Matrix rotate{rotation('z',30,4)};
+    const Matrix move{translation(127,127,0)};
+    const Matrix combined{move*rotate};
+    const Matrix inv_move{inverse(move)};
     for (int iter {0}; iter < 12; ++iter)
     {
-        ppm(static_cast<int>(v(0))+127,static_cast<int>(v(1))+127) = "0 0 0";
-        v = m*v;
+        v = combined*v;
+        ppm(static_cast<int>(v(0)),static_cast<int>(v(1))) = "0 0 0";
+        v = inv_move*v;
     }
     ppm.save("yey");
     return 0;
